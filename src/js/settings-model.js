67b556d32a4664e042508ba7cc1a817ecd272d0e
Matt Keating <matthewkeating@users.noreply.github.com>
2025-01-02 10:06:35 -0500

    Changed the default sidebar visibility to "always"

--- src/js/settings-model.js
+++ src/js/settings-model.js
@@ -1,12 +1,12 @@
 export var showingCompleted = JSON.parse(localStorage.getItem("show_completed")) || false;
-export var tasksSidebarVisibility = JSON.parse(localStorage.getItem("tasks_sidebar_visibility")) || "dbl-click";
+export var tasksSidebarVisibility = JSON.parse(localStorage.getItem("tasks_sidebar_visibility")) || "always";
 export var quickActionsVisibility = JSON.parse(localStorage.getItem("quick_actions_visibility")) || "on-selected";
 export var numCompletedTasksToRetain = JSON.parse(localStorage.getItem("num_completed_tasks_to_retain")) || 20;
 export var numDeletedTasksToRetain = JSON.parse(localStorage.getItem("num_deleted_tasks_to_retain")) || 20;
 
 export function restoreDefaultSettings() {
   setShowingCompleted(false);
-  setTasksSidebarVisibility("dbl-click");
+  setTasksSidebarVisibility("always");
   quickActionsVisibility("on-selected");
   setNumCompletedTasksToRetain(20);
   setNumDeletedTasksToRetain(20);
